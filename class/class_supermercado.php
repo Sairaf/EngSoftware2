27e18de2421cab1d2dca428e1508f8fa6d7f2051
Sairaf <abnergillet16@gmail.com>
2016-09-06 15:11:37 -0300

    Adicionado get e sets da classe

--- class/class_supermercado.php
+++ class/class_supermercado.php
@@ -1,6 +1,30 @@
 <?
 	class supermercado{
-		var $id;
-		var $nomeS;
+		private var $id;
+		private var $nomeSupermercado;
+		private var $descricao;
+		
+		function supermercado($a,$b,$c){
+			$id = $a;
+			$nomeSupermercado = $b;
+			$descricao =  $c;
+		}
+		
+		
+		function setNome($a){
+			$nomeSupermercado = $a
+		}
+		
+		function setDescricao($a){
+			$descricao = $a;
+		}
+		
+		function getNome(){
+			return $nomeSupermercado;
+		}
+		
+		function getDescricao(){
+			return descricao
+		}
 	}
 ?>
