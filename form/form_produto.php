eea10ca668bdd48d89ab7358c9e523881dd8a2cf
Sairaf <abnergillet16@gmail.com>
2016-09-06 01:18:18 -0300

    mais commit no produto

--- form/form_produto.php
+++ form/form_produto.php
@@ -1,12 +1,35 @@
 <?
-
 require("./fachada/fachada_produto.php");
 
   class form_produto(){	
+	var $perfilLogado;
 	
+	var $id;
 	
-	function principal(){
+	function getPerfil(){
+	   return $perfilLogado;
+	}
+	
+	function infoProduto($id){
+		$fachada = new Fachada();
+		$prod = $fachada->getProdutoId($id);
 		
+		if($prod){
+			//adicionar fabricante e data de registro na tabela produto e no class_produto
+		   $nome = $prod->getNome();
+		   $desc = $prod->getDescricao();
+		   $fabricante = $prod->getFabricante();
+		   $preco = $prod->getPreco();
+		   $dataRegistro = $prod->getData();
+		}else{
+		//clocar uma mensagem de erro aqui e redirecionar para a página 
+		}
+	}
+	
+	
+	function cadastroProduto(){
+		$fachada = new Fachada();
+	
 	//$edicao = "";	
 		
 	?>  
@@ -14,6 +37,8 @@ require("./fachada/fachada_produto.php");
 	  <form action="?action_formProduto.php method="post" name="form1">
 	    <td> Nome do produto: <input type="text" id="nomeProduto" name="nome" value=""> 
 		 Descricao: <input type="text" id="nomeProduto" name="descricao" value=""> </td>
+		 Fabricante: <input type="text" id="Fabricante" name="fabricante" value=""> </td>
+		 Data de Registro : <input type="text" id="nomeProduto" name="" value=""><?/*Date*/?> </td>
 	    <td> Preco: <input type="text" id="nomeProduto" name="preco" value=""> </td>
 	    
 	    
