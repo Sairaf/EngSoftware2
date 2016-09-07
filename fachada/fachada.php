ecaa0d76350081aaf114f6147d2ae76307ad33e8
Sairaf <abnergillet16@gmail.com>
2016-09-06 23:56:03 -0300

    Editado fachada

--- fachada/fachada.php
+++ fachada/fachada.php
@@ -1,7 +1,39 @@
 <?
-	class fachada{			
+  require_once(./fachada_produto.php');
+  require_once(./fachada_supermercado.php');
 
+	class fachada{			
+	
+		function validacao($form){
+			switch($form)	{
+				case "produto":
+					
+				break;
+				case "supermercado":
+					
+				break;
+				case "usuario":
+					
+				break;
+				
+					
+			}
 		
+		}
+		
+		function inserir($form){
+			switch($form){
+				case "produto":
+					
+				break;
+				case "supermercado":
+					
+				break;
+				case "usuario":
+					
+				break;
+			}	
+		}
 
 				
 	}
